7a67afbac6fbe36d5c0d0a940da5283dbe87a452
Jayasurya R D <92865629+jayasuryard31@users.noreply.github.com>
2023-11-21 21:30:04 +0530

    Final Push

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const connectDb = require("/config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
-const port =5000;
 
-app.get('api/contacts', (req,res)=> {
-    res.send("Get All Contacts")
-});
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+  console.log(`Server running on port ${port}`);
 });
